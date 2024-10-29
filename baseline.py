3f67b202dd5012c3345f2b2f9a1183ae7cc152c3
Kyle Six <ksl9443@nyu.edu>
2024-10-29 18:03:44 -0400

    Stripped Down baseline

--- baseline.py
+++ baseline.py
@@ -5,16 +5,9 @@ import cv2
 
 import numpy as np
 import os
-import pickle
-from sklearn.cluster import KMeans
 from sklearn.neighbors import BallTree
 
-# TEST line detection and drawing
-from test_img_processing import detect_draw_lines
-
-
 from mazeGraph import MazeGraph, Node
-
 
 # Define a class for a player controlled by keyboard input using pygame
 class KeyboardPlayerPyGame(Player):
@@ -30,16 +23,8 @@ class KeyboardPlayerPyGame(Player):
         self.count = 0  # Counter for saving images
         self.save_dir = "data/images/"  # Directory to save images to
 
-        # Initialize SIFT detector
-        # SIFT stands for Scale-Invariant Feature Transform
-        self.sift = cv2.SIFT_create()
-        # Load pre-trained codebook for VLAD encoding
-        # If you do not have this codebook comment the following line
-        # You can explore the maze once and generate the codebook (refer line 181 onwards for more)
+        # Builds Graph, if it doesn't exist
         self.mazeGraph = MazeGraph()
-        self.codebook = pickle.load(open("codebook.pkl", "rb"))
-        # Initialize database for storing VLAD descriptors of FPV
-        self.database = []
 
     def reset(self):
         # Reset the player state
@@ -79,7 +64,8 @@ class KeyboardPlayerPyGame(Player):
                     self.last_act |= self.keymap[event.key]
                 else:
                     # If a key is pressed that is not mapped to an action, then display target images
-                    self.show_target_images()
+                    #self.show_target_images()
+                    pass
             # Check if a key has been released
             if event.type == pygame.KEYUP:
                 # Check if the released key is in the keymap
@@ -162,166 +148,46 @@ class KeyboardPlayerPyGame(Player):
         cv2.imshow(f"KeyboardPlayer:target_images", concat_img)
         cv2.waitKey(1)
 
+    def find_shortest_path_AND_create_video(self):
+        targets = self.get_target_images()
+        # Return if the target is not set yet
+        if targets is None or len(targets) <= 0:
+            return
+        
+        ######## Calculate shortest path and create video from target image
+        self.mazeGraph.init_navigation(self.get_target_images()[0])
+
     def set_target_images(self, images):
         """
         Set target images
         """
         super(KeyboardPlayerPyGame, self).set_target_images(images)
         self.show_target_images()
-
-    def display_img_from_id(self, id, window_name):
-        """
-        Display image from database based on its ID using OpenCV
-        """
-        path = self.save_dir + str(id) + ".jpg"
-        img = cv2.imread(path)
-        cv2.imshow(window_name, img)
-        cv2.waitKey(1)
-
-    def compute_sift_features(self):
-        """
-        Compute SIFT features for images in the data directory
-        """
-        length = len(os.listdir(self.save_dir))
-        sift_descriptors = list()
-        for i in range(length):
-            path = str(i) + ".jpg"
-            img = cv2.imread(os.path.join(self.save_dir, path))
-            # Pass the image to sift detector and get keypoints + descriptions
-            # We only need the descriptors
-            # These descriptors represent local features extracted from the image.
-            _, des = self.sift.detectAndCompute(img, None)
-            # Extend the sift_descriptors list with descriptors of the current image
-            sift_descriptors.extend(des)
-        return np.asarray(sift_descriptors)
-
-    def get_VLAD(self, img):
-        """
-        Compute VLAD (Vector of Locally Aggregated Descriptors) descriptor for a given image
-        """
-        # We use a SIFT in combination with VLAD as a feature extractor as it offers several benefits
-        # 1. SIFT features are invariant to scale and rotation changes in the image
-        # 2. SIFT features are designed to capture local patterns which makes them more robust against noise
-        # 3. VLAD aggregates local SIFT descriptors into a single compact representation for each image
-        # 4. VLAD descriptors typically require less memory storage compared to storing the original set of SIFT
-        # descriptors for each image. It is more practical for storing and retrieving large image databases efficicently.
-
-        # Pass the image to sift detector and get keypoints + descriptions
-        # Again we only need the descriptors
-        _, des = self.sift.detectAndCompute(img, None)
-        # We then predict the cluster labels using the pre-trained codebook
-        # Each descriptor is assigned to a cluster, and the predicted cluster label is returned
-        pred_labels = self.codebook.predict(des)
-        # Get number of clusters that each descriptor belongs to
-        centroids = self.codebook.cluster_centers_
-        # Get the number of clusters from the codebook
-        k = self.codebook.n_clusters
-        VLAD_feature = np.zeros([k, des.shape[1]])
-
-        # Loop over the clusters
-        for i in range(k):
-            # If the current cluster label matches the predicted one
-            if np.sum(pred_labels == i) > 0:
-                # Then, sum the residual vectors (difference between descriptors and cluster centroids)
-                # for all the descriptors assigned to that clusters
-                # axis=0 indicates summing along the rows (each row represents a descriptor)
-                # This way we compute the VLAD vector for the current cluster i
-                # This operation captures not only the presence of features but also their spatial distribution within the image
-                VLAD_feature[i] = np.sum(
-                    des[pred_labels == i, :] - centroids[i], axis=0
-                )
-        VLAD_feature = VLAD_feature.flatten()
-        # Apply power normalization to the VLAD feature vector
-        # It takes the element-wise square root of the absolute values of the VLAD feature vector and then multiplies
-        # it by the element-wise sign of the VLAD feature vector
-        # This makes the resulting descriptor robust to noice and variations in illumination which helps improve the
-        # robustness of VPR systems
-        VLAD_feature = np.sign(VLAD_feature) * np.sqrt(np.abs(VLAD_feature))
-        # Finally, the VLAD feature vector is normalized by dividing it by its L2 norm, ensuring that it has unit length
-        VLAD_feature = VLAD_feature / np.linalg.norm(VLAD_feature)
-
-        return VLAD_feature
-
-    def get_neighbor(self, img):
-        """
-        Find the nearest neighbor in the database based on VLAD descriptor
-        """
-        # Get the VLAD feature of the image
-        q_VLAD = self.get_VLAD(img).reshape(1, -1)
-        # This function returns the index of the closest match of the provided VLAD feature from the database the tree was created
-        # The '1' indicates the we want 1 nearest neighbor
-        _, index = self.tree.query(q_VLAD, 1)
-        return index[0][0]
-
-    def pre_nav_compute(self):
-        """
-        Build BallTree for nearest neighbor search and find the goal ID
-        """
-        # If this function is called after the game has started
-        if self.count > 0:
-            # below 3 code lines to be run only once to generate the codebook
-            # Compute sift features for images in the database
-            sift_descriptors = self.compute_sift_features()
-
-            # KMeans clustering algorithm is used to create a visual vocabulary, also known as a codebook,
-            # from the computed SIFT descriptors.
-            # n_clusters = 64: Specifies the number of clusters (visual words) to be created in the codebook. In this case, 64 clusters are being used.
-            # init='k-means++': This specifies the method for initializing centroids. 'k-means++' is a smart initialization technique that selects initial
-            # cluster centers in a way that speeds up convergence.
-            # n_init=10: Specifies the number of times the KMeans algorithm will be run with different initial centroid seeds. The final result will be
-            # the best output of n_init consecutive runs in terms of inertia (sum of squared distances).
-            # The fit() method of KMeans is then called with sift_descriptors as input data.
-            # This fits the KMeans model to the SIFT descriptors, clustering them into n_clusters clusters based on their feature vectors
-
-            # TODO: try tuning the function parameters for better performance
-            # codebook = KMeans(
-            #     n_clusters=64, init="k-means++", n_init=10, verbose=1
-            # ).fit(sift_descriptors)
-            # pickle.dump(codebook, open("codebook.pkl", "wb"))
-
-            # Build a BallTree for fast nearest neighbor search
-            # We create this tree to efficiently perform nearest neighbor searches later on which will help us navigate and reach the target location
-
-            # TODO: try tuning the leaf size for better performance
-            tree = BallTree(self.database, leaf_size=60)
-            self.tree = tree
-
-            # Get the neighbor nearest to the front view of the target image and set it as goal
-            targets = self.get_target_images()
-            index = self.get_neighbor(targets[0])
-            self.goal = index
-            print(f"Goal ID: {self.goal}")
-
-    # def pre_navigation(self):
-    #     """
-    #     Computations to perform before entering navigation and after exiting exploration
-    #     """
-    #     super(KeyboardPlayerPyGame, self).pre_navigation()
-    #     self.pre_nav_compute()
-    # if self.count > 0:
-    #     mazeGraph.pre_nav_build_graph(self.tree, self.database, self.save_dir)
-
-    def display_next_best_view(self):
-        """
-        Display the next best view based on the current first-person view
-        """
-
-        # TODO: could you write this function in a smarter way to not simply display the image that closely
-        # matches the current FPV but the image that can efficiently help you reach the target?
-
-        # Get the neighbor of current FPV
-        # In other words, get the image from the database that closely matches current FPV
-        index = self.get_neighbor(self.fpv)
-        # Display the image 5 frames ahead of the neighbor, so that next best view is not exactly same as current FPV
-        self.display_img_from_id(index + 5, f"Next Best View")
-        # Display the next best view id along with the goal id to understand how close/far we are from the goal
-        print(f"Next View ID: {index+5} || Goal ID: {self.goal}")
+        self.find_shortest_path_AND_create_video()
+    
+    def show_video(self):
+        cap = cv2.VideoCapture(self.mazeGraph.path_video_path)
+        while(cap.isOpened()):
+        # Capture each frame
+            # Capture frame-by-frame
+                ret, frame = cap.read()
+                if ret == True:
+                # Display the resulting frame
+                    cv2.imshow('Frame', frame)
+                    
+                # Press Q on keyboard to exit
+                    if cv2.waitKey(55) & 0xFF == ord('q'):
+                        break
+            # Break the loop
+                else:
+                    break
+        cap.release()
+                    
 
     def see(self, fpv):
         """
         Set the first-person view input
         """
-
         # Return if fpv is not available
         if fpv is None or len(fpv.shape) < 3:
             return
@@ -354,33 +220,15 @@ class KeyboardPlayerPyGame(Player):
         if self._state:
             # If in exploration stage
             if self._state[1] == Phase.EXPLORATION:
-                # TODO: could you employ any technique to strategically perform exploration instead of random exploration
-                # to improve performance (reach target location faster)?
-
-                # Get full absolute save path
-                save_dir_full = os.path.join(os.getcwd(), self.save_dir)
-                save_path = save_dir_full + str(self.count) + ".jpg"
-                # Create path if it does not exist
-                if not os.path.isdir(save_dir_full):
-                    os.mkdir(save_dir_full)
-                # Save current FPV
-                cv2.imwrite(save_path, fpv)
-
-                # Get VLAD embedding for current FPV and add it to the database
-                VLAD = self.get_VLAD(self.fpv)
-                self.database.append(VLAD)
-                self.count = self.count + 1
+                pass
             # If in navigation stage
             elif self._state[1] == Phase.NAVIGATION:
-                self.mazeGraph.init_navigation(self.get_target_images()[0])
-                # TODO: could you do something else, something smarter than simply getting the image closest to the current FPV?
-
                 # Key the state of the keys
                 keys = pygame.key.get_pressed()
                 # If 'q' key is pressed, then display the next best view based on the current FPV
                 if keys[pygame.K_q]:
-                    self.display_next_best_view()
-
+                    self.show_target_images()
+                
         # Display the first-person view image on the pygame screen
         rgb = convert_opencv_img_to_pygame(fpv)
         self.screen.blit(rgb, (0, 0))
@@ -391,4 +239,6 @@ if __name__ == "__main__":
     import vis_nav_game
 
     # Start the game with the KeyboardPlayerPyGame player
-    vis_nav_game.play(the_player=KeyboardPlayerPyGame())
+    player = KeyboardPlayerPyGame()
+    vis_nav_game.play(the_player=player)
+
